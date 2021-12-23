7aadb9236dca30a3e6b8df3e8895f374eb169bca
Mariya Kartseva <kjojoj@rambler.ru>
2021-12-23 22:09:48 +0300

    fix zero func

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -10,9 +10,9 @@ const timer = (dadline) => {
         let timeRemaining = (dateStop - dateNow) / 1000;
 
         let days = Math.floor(timeRemaining / 60 / 60 / 24)
-        let hours = Math.floor((timeRemaining / 60 / 60) % 24) < 10 ? '0' + Math.floor((timeRemaining / 60 / 60) % 24) : Math.floor((timeRemaining / 60 / 60) % 24);
-        let minutes = Math.floor((timeRemaining / 60) % 60) < 10 ? '0' + Math.floor((timeRemaining / 60) % 60) : Math.floor((timeRemaining / 60) % 60);
-        let seconds = Math.floor(timeRemaining % 60) < 10 ? '0' + Math.floor(timeRemaining % 60) : Math.floor(timeRemaining % 60);
+        let hours = Math.floor((timeRemaining / 60 / 60) % 24);
+        let minutes = Math.floor((timeRemaining / 60) % 60);
+        let seconds = Math.floor(timeRemaining % 60);
 
         return {
             days,
@@ -22,13 +22,15 @@ const timer = (dadline) => {
             timeRemaining
         }
     }
-
+    const zeroPlus = (number) => {
+        return  number < 10 ? '0' + number : number;
+    }
     const updateClock = () => {
         let gettime = getTimeRemaining();
         if (gettime.timeRemaining > 0) {
-            timerHours.textContent = gettime.hours;
-            timerMinutes.textContent = gettime.minutes;
-            timerSeconds.textContent = gettime.seconds;
+            timerHours.textContent = zeroPlus(gettime.hours);
+            timerMinutes.textContent = zeroPlus(gettime.minutes);
+            timerSeconds.textContent = zeroPlus(gettime.seconds);
             console.log(gettime.timeRemaining);
         }
         else {
