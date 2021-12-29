72870235ddc8578de0df02f6324d24a21b088c5c
Mariya Kartseva <kjojoj@rambler.ru>
2021-12-29 23:51:04 +0300

    fix facken menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,21 +1,22 @@
 const menu = () => {
     const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
+    const body = document.querySelector('body');
+
+    console.log(body);
 
     const handlMenu = () => {
         menu.classList.toggle('active-menu');
     }
 
-    menu.addEventListener('click', (e) => {
-        if (!e.target.closest('menu')) console.log("мимо");
-        
-        if (!e.target.classList.contains('active-menu')) console.log("мимо");
+    body.addEventListener('click', (e) => {
 
-        // if (e.target.closest('li>a') || e.target.closest('.close-btn') || e.target.classList.contains('active-menu'))
-        //     menu.classList.toggle('active-menu');
+        if (e.target.closest('.menu') || e.target.closest('li>a') || e.target.closest('.close-btn') || !e.target.classList.contains('active-menu')) 
+        handlMenu();
+        else console.log("not menu");
+
     })
 
-    menuBtn.addEventListener('click', handlMenu);
 
 }
 export default menu;
