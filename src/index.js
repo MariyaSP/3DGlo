90e616cce8adaa85da53b2f3e41dbae82f73ba88
Mariya Kartseva <kjojoj@rambler.ru>
2022-01-04 00:43:45 +0300

    feat slider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import cost from "./modules/cost";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 
 timer('25 december 2021');
@@ -10,3 +11,7 @@ menu();
 modal();
 cost();
 tabs();
+const portfolioContent = '.portfolio-content';
+const portfolioItem = '.portfolio-item';
+const portfolioDots = '.portfolio-dots';
+slider(portfolioContent, portfolioItem, portfolioDots);
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,18 +1,16 @@
 const menu = () => {
-    const menuBtn = document.querySelector('.menu');
-    const menu = document.querySelector('menu');
+
     const body = document.querySelector('body');
 
-    console.log(body);
-
-    const handlMenu = () => {
-        menu.classList.toggle('active-menu');
-    }
     body.addEventListener('click', (e) => {
+        const menu = document.querySelector('menu');
 
-        if (e.target.closest('.menu') || e.target.closest('li>a') || e.target.closest('.close-btn') || !e.target.classList.contains('active-menu')) 
-        handlMenu();
-        else console.log("not menu");
+        if (e.target.closest('.menu') ||
+            e.target.closest('li>a') ||
+            e.target.closest('.close-btn') ||
+            (!e.target.classList.contains('active-menu') && menu.classList.contains('active-menu'))) {
+            menu.classList.toggle('active-menu');
+        }
     })
 
 }
